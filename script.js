6cd13811afc26fca4d37c1c3d55fce2e0330dbe6
Martina Scibilia <martina.scibilia@outlook.com>
2025-04-23 18:21:22 +0200

    Add search functionality using API

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeather(response) {
+  let temperatureValue = document.querySelector("#temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  let city = response.data.city;
+  let conditionElement = document.querySelector("#condition");
+  let condition = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let dateElement = document.querySelector("#date");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  let icon = `<img src="${response.data.condition.icon_url}" class="temperature-icon" />`;
+
+  iconElement.innerHTML = icon;
+  dateElement.innerHTML = formatDate(date);
+  windElement.innerHTML = `${wind}km/h`;
+  humidityElement.innerHTML = `${humidity}%`;
+  conditionElement.innerHTML = condition;
+  cityElement.innerHTML = city;
+  temperatureValue.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "733615547b11515efo464ab9111t0c1b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+
 function displayForecast() {
   let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
   let forecastHtml = "";
@@ -21,4 +85,8 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHtml;
 }
 
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearch);
+
+searchCity("Rome");
 displayForecast();
